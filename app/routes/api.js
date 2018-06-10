e1828b6969ad006ae7bf856f25b43aebe94614f4
Andrew Sipes <ansipes@live.unc.edu>
2018-06-09 21:44:50 -0400

    Added method for singular GET of User.

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,9 +2,26 @@ const models = require('./../../models')
 
 module.exports = function (server) {
 
+  /*
+    Read all Users
+  */
+
   server.get('/api/users', function (req, res) {
 
     models.User.findAll({}).then(function (result) {
+      res.json(result)
+    })
+
+  })
+
+
+  /*
+    Read User by ID
+  */
+
+  server.get('/api/users/:id', function (req, res) {
+
+    models.User.findById(req.params.id).then(function (result) {
       res.json(result)
     })
     
